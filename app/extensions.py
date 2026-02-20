45ca51b4519b31b04c2d9a944b485a92ecd3384c
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-20 20:13:38 +0430

    new extensions added.

---
+++ app/extensions.py
@@ -0,0 +1,15 @@
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from rich.console import Console
+
+# Instantiate extensions (no app yet)
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+console = Console()
+
+login_manager.login_view = "auth.login"
+login_manager.login_message_category = "info"
