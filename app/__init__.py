117bcdcf7685b0ec67820a99a1af7a12d84d67d2
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-21 13:50:04 +0430

    Fixied.

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,14 @@ import os
 
 from flask import Flask
 
+import app.models as _
+from app.blueprints.admin import bp as admin_bp
+from app.blueprints.api import bp as api_bp
+from app.blueprints.auth import bp as auth_bp
+from app.blueprints.main import bp as main_bp
 from app.config import Config
 from app.extensions import bcrypt, db, login_manager, migrate
+from app.models.permission import Permission
 
 
 def create_app(config_class: type[Config] | None = None) -> Flask:
@@ -17,5 +23,14 @@ def create_app(config_class: type[Config] | None = None) -> Flask:
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+
+    app.register_blueprint(api_bp, url_prefix="/api")
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(admin_bp, url_prefix="/admin")
+    app.register_blueprint(main_bp)
+
+    with app.app_context():
+        if Permission.administer():
+            Permission.refresh()
 
     return app
