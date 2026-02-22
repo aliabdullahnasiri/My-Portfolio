0f163d8e5dc49675a2946d93c916adc12882a185
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 12:21:03 +0430

    Fixied.

--- app/config.py
+++ app/config.py
@@ -10,22 +10,28 @@ load_dotenv()
 class Config:
     PROJECT_TITLE: str = os.getenv("PROJECT_TITLE", "Flask Portfolio")
 
-    SECRET_KEY = os.getenv("SECRET_KEY", "12345678")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "12345678")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+    SQLALCHEMY_DATABASE_URI: str = os.getenv("DATABASE_URL", "sqlite:///dev.db")
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
 
-    SQLALCHEMY_ECHO = os.getenv("SQLALCHEMY_ECHO", "false").lower() == "true"
+    SQLALCHEMY_ECHO: bool = os.getenv("SQLALCHEMY_ECHO", "false").lower() == "true"
 
-    APP_DIR = "app"
+    APP_DIR: str = "app"
 
-    DEFAULT_AVATAR = "assets/img/default-avatar.png"
+    DEFAULT_AVATAR: str = "assets/img/default-avatar.png"
 
-    UPLOAD_FOLDER = os.path.join("app", "static", "uploads")
+    UPLOAD_FOLDER: str = os.path.join("app", "static", "uploads")
 
     FLASKY_ADMIN: str = "nasiri.aliabdullah@gmail.com"
 
     ADMINISTRATOR: str = "ADMINISTRATOR"
 
     ADMINISTER: str = "ADMINISTER"
+
+    CURRENCY_SYMBOL: str = chr(36)
+
+    DEFAULT_AVATAR: str = "admin/assets/img/default-avatar.png"
+
+    DEVELOPER: str = "Ali Abdullah Nasiri"
