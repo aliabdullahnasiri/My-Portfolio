0396a3055711ea7277c6b92e821bde3479302ed6
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 20:15:29 +0430

    New functions have been added.

--- app/func.py
+++ app/func.py
@@ -1,5 +1,53 @@
 import pathlib
 import re
+from typing import Union
+
+from flask import render_template
+
+from app.config import Config
+from app.models.file import File
+
+
+def get_file(id: int) -> Union[File, None]:
+    return File.query.filter_by(id=id).first()
+
+
+def get_file_url(id: int) -> Union[str, None]:
+    file = File.query.filter_by(id=id).first()
+
+    if file:
+        return file.file_url
+
+
+def render_td(col_id: str, obj, max_length: int = 64) -> str:
+    dct = obj.to_dict()
+
+    dct.setdefault("obj_{}".format(obj.__class__.__name__.lower()), obj)
+
+    for TEMP in Config.TD_TEMPS:
+        if col_id.startswith(let := "temp_"):
+            col_id = col_id[len(let) :]
+
+        if col_id == TEMP.name.split(chr(46)).pop(0):
+            try:
+                return render_template(
+                    f"admin/components/tables/td/{TEMP.name}",
+                    **{col_id: getattr(obj, col_id)},
+                )
+            except AttributeError as _:
+                pass
+
+            return render_template(f"admin/components/tables/td/{TEMP.name}", **dct)
+
+    if hasattr(obj, attr := f"display_{col_id}"):
+        return getattr(obj, attr)
+
+    val = dct.get(col_id, "N/A")
+
+    if type(val) == str and len(val) > max_length:
+        return "{}...".format(val[slice(max_length)])
+
+    return val
 
 
 def __import_all__(path: str) -> None:
