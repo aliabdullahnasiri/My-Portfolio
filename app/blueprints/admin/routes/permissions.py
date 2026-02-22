c610112a48ad4c1f46390a7e10265735e53f2cc6
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 23:18:59 +0430

    Fixied.

--- app/blueprints/admin/routes/permissions.py
+++ app/blueprints/admin/routes/permissions.py
@@ -1,8 +1,10 @@
 from flask import render_template
 
 from app.blueprints.admin import bp
+from app.models.user import admin_required
 
 
 @bp.get("/permissions")
+@admin_required
 def permissions():
     return render_template("admin/pages/permissions.html", title="Permissions")
