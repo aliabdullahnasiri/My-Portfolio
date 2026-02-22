1b6d8cc764683da3434c1c50739363fadeca65b3
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 23:18:51 +0430

    Fixied.

--- app/blueprints/admin/__init__.py
+++ app/blueprints/admin/__init__.py
@@ -1,14 +1,8 @@
 from flask import Blueprint
 
 from app.func import __import_all__
-from app.models.user import admin_required
 
 bp = Blueprint("admin", __name__)
-
-
-@bp.before_request
-@admin_required
-def _(): ...
 
 
 __import_all__("app/blueprints/admin/routes")
