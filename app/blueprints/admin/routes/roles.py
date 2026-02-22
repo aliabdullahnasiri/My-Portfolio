c9d3fcb4d76d92f4ab75538edbc6a65ef74f1a27
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 23:19:02 +0430

    Fixied.

--- app/blueprints/admin/routes/roles.py
+++ app/blueprints/admin/routes/roles.py
@@ -1,8 +1,10 @@
 from flask import render_template
 
 from app.blueprints.admin import bp
+from app.models.user import admin_required
 
 
 @bp.get("/roles")
+@admin_required
 def roles():
     return render_template("admin/pages/roles.html", title="Roles")
