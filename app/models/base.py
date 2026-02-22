920fabe2f9cb22acd2582a4bea984319f10cc06b
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 20:15:50 +0430

    Fixied.

--- app/models/base.py
+++ app/models/base.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import humanize
 from flask import request
 from numerize import numerize
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String, event
 from sqlalchemy.ext.declarative import declared_attr
 
 from app.extensions import console, db
@@ -15,6 +15,10 @@ class Base(db.Model):
     @declared_attr
     def id(cls):
         return Column(Integer, primary_key=True, autoincrement=True)
+
+    @declared_attr
+    def uid(cls):
+        return Column(String(8), primary_key=False, unique=True)
 
     # Timestamps
     created_at = db.Column(
@@ -56,9 +60,19 @@ class Base(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
+            "uid": getattr(self, "uid") if hasattr(self, "uid") else None,
             "created_at": self.display_created_at,
             "updated_at": self.display_updated_at,
         }
+
+
+@event.listens_for(Base, "before_insert", propagate=True)
+def generate_uid(mapper, connection, target):
+    cls = target.__class__
+    obj = cls.query.order_by(cls.id.desc()).first()
+    prefix = target.__class__.__name__[0].upper()
+    val = "{}-{:>06}".format(prefix, (obj.id if obj else 0) + 1)
+    target.uid = val if not len(val) > 8 else None
 
 
 def all(self):
