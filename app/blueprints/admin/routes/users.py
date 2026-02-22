314c392b2338893b72363dc4f990ec727902bae1
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 23:19:04 +0430

    Fixied.

--- app/blueprints/admin/routes/users.py
+++ app/blueprints/admin/routes/users.py
@@ -1,8 +1,10 @@
 from flask import render_template
 
 from app.blueprints.admin import bp
+from app.models.user import admin_required
 
 
 @bp.get("/users")
+@admin_required
 def users():
     return render_template("admin/pages/users.html", title="Users")
