52cd5656feccd1af797a9c40bb1c36190662961b
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 18:25:58 +0430

    user form has been added.

---
+++ app/forms/user.py
@@ -0,0 +1,122 @@
+import json
+import re
+from operator import and_
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from wtforms import (
+    DateField,
+    FileField,
+    HiddenField,
+    MultipleFileField,
+    PasswordField,
+    StringField,
+    SubmitField,
+    ValidationError,
+)
+from wtforms.validators import DataRequired, Email, Length, Optional
+
+from app.extensions import db
+from app.models.phone import Phone
+from app.models.role import Role
+from app.models.user import User
+
+
+class AddUserForm(FlaskForm):
+    first_name = StringField("First Name", validators=[Length(max=50)])
+    middle_name = StringField("Middle Name", validators=[Length(max=50)])
+    last_name = StringField("Last Name", validators=[Length(max=50)])
+    user_name = StringField("Username", validators=[DataRequired(), Length(max=50)])
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email(message="Enter a valid email address")],
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    birthday = DateField("Birthday", format="%Y-%m-%d", validators=[Optional()])
+    avatar = FileField("Upload new profile picture.")
+
+    files = MultipleFileField("Files")
+    phones = StringField("Phone", validators=[Optional()])
+    roles = StringField("Roles", validators=[Optional()])
+
+    submit = SubmitField("Add")
+
+    def validate_user_name(self, user_name):
+        if User.query.filter_by(user_name=user_name.data).first():
+            raise ValidationError("Username already taken.")
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError("Email already registered.")
+
+    def validate_phones(self, phones):
+        nums = json.loads(phones.data)
+
+        for num in nums:
+            if (
+                db.session.query(Phone)
+                .filter(
+                    Phone.number == num,
+                )
+                .first()
+            ):
+
+                raise ValidationError(f"Duplicate entry {num!r} for phone number!")
+
+    def validate_roles(self, roles):
+        roles = json.loads(roles.data)
+        pattern: re.Pattern = re.compile(r"^R.\d{6}$")
+
+        for role in roles:
+            if not pattern.search(role):
+                raise ValidationError(f"Not a valid Role ID.")
+            elif not Role.query.filter_by(uid=role).first():
+                raise ValidationError("Role with the given ID was not found :(")
+
+
+class UpdateUserForm(AddUserForm):
+    uid = HiddenField("UID", validators=[DataRequired()])
+    user_uid = HiddenField("User UID", validators=[DataRequired()])
+
+    password = PasswordField("Password")
+
+    submit = SubmitField("Update")
+
+    def validate_user_name(self, user_name):
+        if (
+            db.session.query(User)
+            .filter(
+                and_(
+                    getattr(User, "uid") != self.user_uid.data,
+                    User.user_name == user_name.data,
+                )
+            )
+            .first()
+        ):
+            raise ValidationError("Username already taken!")
+
+    def validate_email(self, email):
+        if User.query.filter(
+            and_(getattr(User, "uid") != self.user_uid.data, User.email == email.data)
+        ).first():
+            raise ValidationError("Email already registered!")
+
+    def validate_phones(self, phones):
+        nums = json.loads(phones.data)
+
+        if hasattr(self, "user_uid"):
+            uid = self.user_uid.data
+
+            for num in nums:
+                if (
+                    db.session.query(Phone)
+                    .filter(
+                        and_(
+                            Phone.user_id != uid,
+                            Phone.number == num,
+                        )
+                    )
+                    .first()
+                ):
+
+                    raise ValidationError(f"Duplicate entry {num!r} for phone number!")
