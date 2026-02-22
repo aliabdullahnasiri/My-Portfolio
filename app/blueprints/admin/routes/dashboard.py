2cc21642dd5d2c6b0eb276be29b7c21b20ef5442
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-22 12:11:53 +0430

    Fixied.

--- app/blueprints/admin/routes/dashboard.py
+++ app/blueprints/admin/routes/dashboard.py
@@ -3,7 +3,6 @@ from flask import render_template
 from app.blueprints.admin import bp
 
 
-@bp.get("/")
 @bp.get("/dashboard")
 def dashboard():
     return render_template("admin/pages/dashboard.html", title="Dashboard")
