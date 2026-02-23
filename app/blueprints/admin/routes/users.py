8f11cd71564ebdcdbcad368e7996ecd3fcb23970
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-23 11:09:23 +0430

    Fixied.

--- app/blueprints/admin/routes/users.py
+++ app/blueprints/admin/routes/users.py
@@ -1,10 +1,15 @@
 from flask import render_template
 
 from app.blueprints.admin import bp
+from app.forms.user import AddUserForm, UpdateUserForm
 from app.models.user import admin_required
 
 
 @bp.get("/users")
 @admin_required
 def users():
-    return render_template("admin/pages/users.html", title="Users")
+    return render_template(
+        "admin/pages/users.html",
+        title="Users",
+        form={"a": AddUserForm(), "u": UpdateUserForm()},
+    )
