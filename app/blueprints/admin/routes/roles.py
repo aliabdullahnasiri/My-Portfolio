815bd891d2ae0fe9a57f1a2d250a415b21e5a764
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-24 12:22:47 +0430

    Fixied.

--- app/blueprints/admin/routes/roles.py
+++ app/blueprints/admin/routes/roles.py
@@ -1,10 +1,18 @@
 from flask import render_template
 
 from app.blueprints.admin import bp
+from app.forms.role import AddRoleForm, UpdateRoleForm
 from app.models.user import admin_required
 
 
 @bp.get("/roles")
 @admin_required
 def roles():
-    return render_template("admin/pages/roles.html", title="Roles")
+    return render_template(
+        "admin/pages/roles.html",
+        title="Roles",
+        form={
+            "a": AddRoleForm(),
+            "u": UpdateRoleForm(),
+        },
+    )
