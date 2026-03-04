c54424c7f3b512eb1ab4020f2b20b52f71b4dc74
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-04 14:14:00 +0430

    Fixied.

--- app/blueprints/api/routes/upload.py
+++ app/blueprints/api/routes/upload.py
@@ -63,7 +63,7 @@ def upload() -> Response:
         f.user_uid = user.uid
         f.file_name = request.form.get("filename", filename)
         f.file_description = request.form.get("file_description")
-        f.file_for = request.form.get("file_for", FileForEnum.AVATAR.value)
+        f.file_for = request.form.get("file_for", FileForEnum.REGULAR.value)
         f.file_url = url_for("static", filename=f"uploads/{today}/{filename}")
 
         db.session.add(f)
