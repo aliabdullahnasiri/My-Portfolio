fc818d26699a68c1b25c108312a72ea4eb14a1c0
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-04 14:14:09 +0430

    Fixied.

---
+++ app/blueprints/api/routes/profile.py
@@ -0,0 +1,231 @@
+import json
+from typing import Dict, List, Tuple, Union
+
+from flask import Response, request, url_for
+from flask_login import current_user, login_required
+
+from app.blueprints.api import bp
+from app.const import DEFAULT_AVATAR
+from app.extensions import db
+from app.forms.profile import AddProfileForm, UpdateProfileForm
+from app.func import get_file, get_file_url, render_td
+from app.models.permission import Permission
+from app.models.profile import Profile
+from app.models.user import permission_required
+from app.types import ColumnID, ColumnName
+
+cols: List[Tuple[ColumnID, ColumnName]] = [
+    (ColumnID("uid"), ColumnName("UID")),
+]
+
+
+@bp.get("/fetch/profiles")
+@login_required
+@permission_required(Permission.get("FETCH_PROFILES"))
+def fetch_profiles() -> Response:
+    profiles: List[Profile] = [profile.to_dict() for profile in Profile.query.all()]
+
+    response: Response = Response(
+        json.dumps(profiles), headers={"Content-Type": "application/json"}
+    )
+    response.status_code = 200
+
+    return response
+
+
+@bp.get("/fetch/rows/profiles")
+@login_required
+@permission_required(Permission.get("FETCH_PROFILES"))
+def fetch_profiles_rows() -> Response:
+    profiles: List[Profile] = Profile.query.all()
+
+    rows: List[List] = []
+
+    for profile in profiles:
+        row = [render_td(col_id, profile) for col_id, _ in cols]
+        rows.append(row)
+
+    dct: Dict = {
+        "cols": cols,
+        "rows": rows,
+    }
+
+    response: Response = Response(
+        json.dumps(dct),
+        status=200,
+        headers={"Content-Type": "application/json"},
+    )
+
+    return response
+
+
+@bp.get("/fetch/row/profile/<string:uid>")
+@login_required
+@permission_required(Permission.get("FETCH_PROFILE"))
+def fetch_profile_row(uid) -> Response:
+    response: Response = Response()
+
+    profile: Union[Profile, None] = Profile.query.filter_by(uid=uid).first()
+
+    if profile:
+        response.response = json.dumps(
+            {
+                key: val
+                for key, val in zip(
+                    [col_id for col_id, _ in cols],
+                    [render_td(col_id, profile) for col_id, _ in cols],
+                )
+            }
+        )
+        response.status_code = 200
+
+    else:
+        dct = {
+            "message": "Profile with the given ID was not found :(",
+            "category": "error",
+        }
+
+        response.response = json.dumps(dct)
+        response.status_code = 404
+
+    return response
+
+
+@bp.get("/fetch/profile/<string:uid>")
+@login_required
+@permission_required(Permission.get("FETCH_PROFILE"))
+def fetch_profile(uid) -> Response:
+    profile = Profile.query.filter_by(uid=uid).first()
+
+    if profile:
+        response: Response = Response(
+            json.dumps(profile.to_dict()),
+            status=200,
+            headers={"Content-Type": "application/json"},
+        )
+
+        return response
+
+    return Response(
+        json.dumps(
+            {
+                "message": "Profile with the given ID was not found :(",
+                "category": "error",
+            }
+        ),
+        headers={"Content-Type": "application/json"},
+        status=404,
+    )
+
+
+@bp.post("/update/profile")
+@login_required
+@permission_required(Permission.get("FETCH_PROFILE") | Permission.get("UPDATE_PROFILE"))
+def update_profile() -> Response:
+    form = UpdateProfileForm()
+
+    response: Dict = {}
+
+    if form.validate_on_submit():
+        profile = Profile.query.filter_by(uid=form.uid.data).first()
+        print(request.form)
+
+        if profile:
+            profile.full_name = form.full_name.data
+            profile.headline = form.headline.data
+            profile.bio = form.bio.data
+            profile.short_bio = form.short_bio.data
+            profile.public_email = form.public_email.data
+            profile.phone = form.phone.data
+            profile.years_of_experience = form.years_of_experience.data
+            profile.is_active = form.is_active.data
+
+            if files := request.form.get("files"):
+                try:
+                    files = json.loads(files)
+
+                    if fid := files.get("avatar"):
+                        profile.avatar_url = get_file_url(fid)
+
+                    if fid := files.get("resume", [None])[-1]:
+                        profile.resume_url = get_file_url(fid)
+
+                except json.JSONDecodeError as err:
+                    print(err)
+
+            db.session.commit()
+
+            response["title"] = "Good job!"
+            response["category"] = "success"
+            response["message"] = "Profile updated successfully!"
+
+    else:
+        response["errors"] = form.errors
+
+    return Response(
+        json.dumps(response), headers={"Content-Type": "application/json"}, status=200
+    )
+
+
+@bp.delete("/delete/profile/<string:uid>")
+@login_required
+@permission_required(Permission.get("FETCH_PROFILE") | Permission.get("DELETE_PROFILE"))
+def delete_profile(uid):
+    response = {}
+
+    if profile := Profile.query.filter_by(uid=uid).first():
+        db.session.delete(profile)
+        db.session.commit()
+
+        response["title"] = "Deleted!"
+        response["message"] = "Profile deleted successfully"
+        response["category"] = "success"
+        response["status"] = 200
+
+    else:
+        response["title"] = "Error :("
+        response["message"] = "Profile not found"
+        response["category"] = "error"
+        response["status"] = 404
+
+    return Response(
+        json.dumps(response),
+        status=response["status"],
+        headers={"Content-Type": "application/json"},
+    )
+
+
+@bp.post("/add/profile")
+@login_required
+@permission_required(Permission.get("CREATE_PROFILE"))
+def add_profile() -> Response:
+    form = AddProfileForm()
+
+    response: Dict = {}
+
+    if form.validate_on_submit():
+        profile = Profile()
+
+        profile.user_id = current_user.id
+        profile.full_name = form.full_name.data
+        profile.headline = form.headline.data
+        profile.bio = form.bio.data
+        profile.short_bio = form.short_bio.data
+        profile.public_email = form.public_email.data
+        profile.phone = form.phone.data
+        profile.years_of_experience = form.years_of_experience.data
+
+        profile.is_active = False
+
+        db.session.add(profile)
+        db.session.commit()
+
+        response["message"] = "Profile added successfully"
+        response["category"] = "success"
+        response["title"] = "Profile Added"
+        response["id"] = getattr(profile, "uid")
+
+    else:
+        response["errors"] = form.errors
+
+    return Response(json.dumps(response))
