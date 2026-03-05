abc13236f1ff5317f86cabfed95115776aba3bec
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-05 09:22:04 +0430

    Fixied.

--- app/blueprints/main/routes/home.py
+++ app/blueprints/main/routes/home.py
@@ -1,7 +1,16 @@
-from flask import Response
+from flask import render_template
 
 from app.blueprints.main import bp
+from app.models.profile import Profile
 
 
 @bp.get("/")
-def home() -> Response: ...
+@bp.get("/home")
+def home():
+    return render_template(
+        "main/pages/home.html",
+        title="Home",
+        profile=Profile.query.filter_by(is_active=True)
+        .order_by(getattr(Profile, "updated_at").desc())
+        .first(),
+    )
