76b85faef699990bc1997c40dc9c05a2dbc7f6c7
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-07 10:05:40 +0430

    Fixied.

--- app/blueprints/admin/routes/certificates.py
+++ app/blueprints/admin/routes/certificates.py
@@ -1,10 +1,15 @@
 from flask import render_template
 
 from app.blueprints.admin import bp
+from app.forms.certificate import AddCertificateForm, UpdateCertificateForm
 from app.models.user import admin_required
 
 
 @bp.get("/certificates")
 @admin_required
 def certificates():
-    return render_template("admin/pages/certificates.html", title="Certificates")
+    return render_template(
+        "admin/pages/certificates.html",
+        title="Certificates",
+        form={"a": AddCertificateForm(), "u": UpdateCertificateForm()},
+    )
