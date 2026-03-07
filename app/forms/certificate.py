70374c4bd3b230c32532f13eadd5c12b59226b9a
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-07 10:30:10 +0430

    New input has been added.

--- app/forms/certificate.py
+++ app/forms/certificate.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 from flask_wtf import FlaskForm
 from wtforms import (
     BooleanField,
@@ -8,11 +11,16 @@ from wtforms import (
     StringField,
     SubmitField,
     TextAreaField,
+    ValidationError,
 )
 from wtforms.validators import URL, DataRequired, Length, Optional
 
+from app.models.profile import Profile
+
 
 class AddCertificateForm(FlaskForm):
+
+    profile_uid = StringField("Profile UID", validators=[DataRequired()])
 
     title = StringField("Title", validators=[DataRequired(), Length(max=255)])
 
@@ -40,14 +48,22 @@ class AddCertificateForm(FlaskForm):
 
     file = FileField("File")
 
-    is_featured = BooleanField("Featured")
-    is_public = BooleanField("Public")
-
     display_order = IntegerField("Display Order", validators=[Optional()])
 
     submit = SubmitField("Add")
 
+    def validate_profile_uid(self, profile_uid):
+        pattern: re.Pattern = re.compile(r"^P.\d{6}$")
+        if not pattern.search(profile_uid.data):
+            raise ValidationError(f"Not a valid Profile UID.")
+        elif not Profile.query.filter_by(uid=profile_uid.data).first():
+            raise ValidationError("Profile with the given UID was not found :(")
+
 
 class UpdateCertificateForm(AddCertificateForm):
     uid = HiddenField("UID", validators=[DataRequired()])
+
+    is_featured = BooleanField("Featured")
+    is_public = BooleanField("Public")
+
     submit = SubmitField("Update")
