73413e2a864182e59fad4423666570e5170fcd5f
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-08 11:16:47 +0430

    The social link model has been added.

---
+++ app/models/social_link.py
@@ -0,0 +1,23 @@
+from app.extensions import db
+
+
+class SocialLink(db.Model):
+    __tablename__ = "social_links"
+
+    profile_uid = db.Column(db.Integer, db.ForeignKey("profiles.uid"), nullable=False)
+
+    platform = db.Column(db.String(50), nullable=False)
+
+    url = db.Column(db.String(255), nullable=False)
+
+    icon = db.Column(db.String(100), nullable=True)
+
+    order = db.Column(db.Integer, default=0)
+
+    is_visible = db.Column(db.Boolean, default=True)
+
+    # relationship
+    profile = db.relationship(
+        "Profile",
+        backref=db.backref("social_links", lazy=True, cascade="all, delete-orphan"),
+    )
