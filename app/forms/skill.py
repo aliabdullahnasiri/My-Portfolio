33c3514b2921aa65bb39fd3cae246e65f951a30a
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-09 10:29:04 +0430

    modified.

--- app/forms/skill.py
+++ app/forms/skill.py
@@ -1,25 +1,44 @@
-import json
-import re
-
 from flask_wtf import FlaskForm
-from sqlalchemy import and_
 from wtforms import (
     BooleanField,
     HiddenField,
+    IntegerField,
     StringField,
     SubmitField,
     TextAreaField,
-    ValidationError,
 )
-from wtforms.validators import DataRequired, Length, Optional
-
-from app.models.permission import Permission
-from app.models.role import Role
+from wtforms.validators import DataRequired, Length, NumberRange, Optional
 
 
 class AddSkillForm(FlaskForm):
-    pass
+    name = StringField(
+        "Skill Name", validators=[DataRequired(), Length(min=2, max=100)]
+    )
+
+    category = StringField(
+        "Category", validators=[DataRequired(), Length(min=2, max=100)]
+    )
+
+    icon = StringField(
+        "Icon Class",
+        validators=[Optional(), Length(max=100)],
+        description="Example: fa-brands fa-python",
+    )
+
+    level = IntegerField(
+        "Skill Level (%)",
+        validators=[Optional(), NumberRange(min=0, max=100)],
+        description="Skill proficiency from 0 to 100",
+    )
+
+    description = TextAreaField("Description", validators=[Optional(), Length(max=500)])
+
+    is_featured = BooleanField("Featured Skill")
+
+    submit = SubmitField("Add")
 
 
-class UpdateSkillForm(FlaskForm):
-    pass
+class UpdateSkillForm(AddSkillForm):
+    uid = HiddenField("Skill UID", validators=[DataRequired()])
+
+    submit = SubmitField("Update")
