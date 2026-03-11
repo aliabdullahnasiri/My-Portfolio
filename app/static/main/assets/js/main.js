d5b00b5cad96d0b7d637ca043da95626ee80196e
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-11 12:28:51 +0430

    Fixied.

--- app/static/main/assets/js/main.js
+++ app/static/main/assets/js/main.js
@@ -39,12 +39,17 @@ navClone();
 
 $(".d2c_certificate_slider").slick({
   dots: true,
-  arrows: false,
+  arrows: !false,
   infinite: true,
   autoplay: true,
   speed: 1500,
   slidesToShow: 3,
   slidesToScroll: 1,
+
+  prevArrow:
+    '<button type="button" class="d2c_carousel_left_btn" aria-label="carousel-control"><i class="fas fa-arrow-left"></i></button>',
+  nextArrow:
+    '<button type="button" class="d2c_carousel_right_btn" aria-label="carousel-control"><i class="fas fa-arrow-right"></i></button>',
   responsive: [
     {
       breakpoint: 1400,
