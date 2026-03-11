ba5cbe2a05679942230303cfed2166137b5859c2
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-11 21:42:42 +0430

    Fixied.

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -4,8 +4,10 @@ import re
 from flask_wtf import FlaskForm
 from wtforms import ValidationError
 
+from app.models.phone import Phone
 from app.models.profile import Profile
 from app.models.role import Permission, Role
+from app.models.user import User
 
 
 class Form(FlaskForm):
@@ -32,3 +34,35 @@ class Form(FlaskForm):
         for permission in permissions:
             if not Permission.query.filter_by(uid=permission).first():
                 raise ValidationError("Permission with the given ID was not found :(")
+
+    def validate_user_name(self, user_name):
+        if User.query.filter_by(user_name=user_name.data).first():
+            raise ValidationError("Username already exists.")
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError("Email already exists.")
+
+    def validate_phones(self, phones):
+        nums = json.loads(phones.data)
+
+        for num in nums:
+            if (
+                db.session.query(Phone)
+                .filter(
+                    Phone.number == num,
+                )
+                .first()
+            ):
+
+                raise ValidationError(f"Duplicate entry {num!r} for phone number!")
+
+    def validate_roles(self, roles):
+        roles = json.loads(roles.data)
+        pattern: re.Pattern = re.compile(r"^R.\d{6}$")
+
+        for role in roles:
+            if not pattern.search(role):
+                raise ValidationError(f"Not a valid Role ID.")
+            elif not Role.query.filter_by(uid=role).first():
+                raise ValidationError("Role with the given ID was not found :(")
