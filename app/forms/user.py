8af4126032187bfe6954a784f6b99059df5cf012
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-11 21:42:54 +0430

    Fixied.

--- app/forms/user.py
+++ app/forms/user.py
@@ -1,8 +1,6 @@
 import json
-import re
 from operator import and_
 
-from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import (
     DateField,
@@ -17,12 +15,12 @@ from wtforms import (
 from wtforms.validators import DataRequired, Email, Length, Optional
 
 from app.extensions import db
+from app.forms import Form
 from app.models.phone import Phone
-from app.models.role import Role
 from app.models.user import User
 
 
-class AddUserForm(FlaskForm):
+class AddUserForm(Form):
     first_name = StringField("First Name", validators=[Length(max=50)])
     middle_name = StringField("Middle Name", validators=[Length(max=50)])
     last_name = StringField("Last Name", validators=[Length(max=50)])
@@ -40,38 +38,6 @@ class AddUserForm(FlaskForm):
     roles = StringField("Roles", validators=[Optional()])
 
     submit = SubmitField("Add")
-
-    def validate_user_name(self, user_name):
-        if User.query.filter_by(user_name=user_name.data).first():
-            raise ValidationError("Username already taken.")
-
-    def validate_email(self, email):
-        if User.query.filter_by(email=email.data).first():
-            raise ValidationError("Email already registered.")
-
-    def validate_phones(self, phones):
-        nums = json.loads(phones.data)
-
-        for num in nums:
-            if (
-                db.session.query(Phone)
-                .filter(
-                    Phone.number == num,
-                )
-                .first()
-            ):
-
-                raise ValidationError(f"Duplicate entry {num!r} for phone number!")
-
-    def validate_roles(self, roles):
-        roles = json.loads(roles.data)
-        pattern: re.Pattern = re.compile(r"^R.\d{6}$")
-
-        for role in roles:
-            if not pattern.search(role):
-                raise ValidationError(f"Not a valid Role ID.")
-            elif not Role.query.filter_by(uid=role).first():
-                raise ValidationError("Role with the given ID was not found :(")
 
 
 class UpdateUserForm(AddUserForm):
