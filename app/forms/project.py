2505e1f02ad3e2cbf95ada89cab379ebbeff69a7
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-11 14:25:40 +0430

    The project form object has been added.

---
+++ app/forms/project.py
@@ -0,0 +1,70 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    BooleanField,
+    DateField,
+    HiddenField,
+    IntegerField,
+    SelectField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+    URLField,
+)
+from wtforms.validators import URL, DataRequired, Length, Optional
+
+
+class AddProjectForm(FlaskForm):
+
+    profile_uid = StringField("Profile UID", validators=[DataRequired()])
+
+    title = StringField("Project Title", validators=[DataRequired(), Length(max=150)])
+
+    slug = StringField("Slug", validators=[Optional(), Length(max=180)])
+
+    short_description = StringField(
+        "Short Description", validators=[Optional(), Length(max=300)]
+    )
+
+    description = TextAreaField("Full Description", validators=[Optional()])
+
+    github_url = URLField("GitHub Repository", validators=[Optional(), URL()])
+
+    demo_url = URLField("Live Demo", validators=[Optional(), URL()])
+
+    documentation_url = URLField("Documentation", validators=[Optional(), URL()])
+
+    project_type = SelectField(
+        "Project Type",
+        choices=[
+            ("web", "Web Application"),
+            ("tool", "Security Tool"),
+            ("script", "Script"),
+            ("research", "Research"),
+        ],
+        validators=[Optional()],
+    )
+
+    status = SelectField(
+        "Project Status",
+        choices=[
+            ("completed", "Completed"),
+            ("ongoing", "Ongoing"),
+        ],
+        validators=[Optional()],
+    )
+
+    start_date = DateField("Start Date", validators=[Optional()])
+    end_date = DateField("End Date", validators=[Optional()])
+
+    display_order = IntegerField("Display Order")
+
+    submit = SubmitField("Add")
+
+
+class UpdateProjectForm(AddProjectForm):
+    uid = HiddenField("UID", validators=[DataRequired()])
+
+    is_featured = BooleanField("Show this item as Featured")
+    is_public = BooleanField("Make this item Publicly Visible")
+
+    submit = SubmitField("Update")
