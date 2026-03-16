0d4741e8dbc62cf2d683ad57d3b260fdad0cc51d
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-16 15:29:03 +0430

    Completed.

--- app/forms/experience.py
+++ app/forms/experience.py
@@ -1,11 +1,49 @@
-from wtforms import HiddenField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import (
+    BooleanField,
+    DateField,
+    HiddenField,
+    SelectField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+)
+from wtforms.validators import URL, DataRequired, Length, Optional
 
 from app.forms import Form
 
 
 class AddExperienceForm(Form):
     profile_uid = StringField("Profile UID", validators=[DataRequired()])
+
+    company = StringField("Company", validators=[DataRequired(), Length(max=150)])
+
+    position = StringField("Position", validators=[DataRequired(), Length(max=150)])
+
+    employment_type = SelectField(
+        "Employment Type",
+        choices=[
+            ("full_time", "Full Time"),
+            ("part_time", "Part Time"),
+            ("freelance", "Freelance"),
+            ("internship", "Internship"),
+            ("contract", "Contract"),
+        ],
+        validators=[Optional()],
+    )
+
+    location = StringField("Location", validators=[Optional(), Length(max=120)])
+
+    company_url = StringField("Company Website", validators=[Optional(), URL()])
+
+    start_date = DateField("Start Date", validators=[Optional()])
+
+    end_date = DateField("End Date", validators=[Optional()])
+
+    is_current = BooleanField("Currently Working Here")
+
+    description = TextAreaField(
+        "Responsibilities / Description", validators=[Optional(), Length(max=2000)]
+    )
 
     submit = SubmitField("Add")
 
