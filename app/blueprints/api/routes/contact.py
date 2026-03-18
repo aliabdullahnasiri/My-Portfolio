49e17300f354b18930bfed95003513839d9840d6
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-18 11:51:17 +0430

    Modified.

---
+++ app/blueprints/api/routes/contact.py
@@ -0,0 +1,38 @@
+from typing import Dict
+
+from flask import jsonify
+
+from app.blueprints.api import bp
+from app.extensions import db
+from app.forms.contact import ContactForm
+from app.models.contact import ContactMessage
+
+
+@bp.post("/contact")
+def contact():
+    form = ContactForm()
+
+    response: Dict = {}
+
+    if form.validate_on_submit():
+        m = ContactMessage()
+
+        m.first_name = form.first_name.data
+        m.last_name = form.last_name.data
+        m.email = form.email.data
+        m.message = form.message.data
+
+        db.session.add(m)
+        db.session.commit()
+
+        response.update(
+            **{
+                "status": "success",
+                "message": "Message sent successfully",
+            }
+        )
+
+    else:
+        response["errors"] = form.errors
+
+    return jsonify(response)
