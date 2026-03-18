44df2e6347316ac32155f9d542f5267b79c54d7e
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-18 11:40:31 +0430

    New form has been added.

---
+++ app/forms/contact.py
@@ -0,0 +1,22 @@
+from wtforms import EmailField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+from app.forms import Form
+
+
+class ContactForm(Form):
+    first_name = StringField(
+        "First Name", validators=[DataRequired(), Length(min=2, max=100)]
+    )
+
+    last_name = StringField(
+        "Last Name", validators=[DataRequired(), Length(min=2, max=100)]
+    )
+
+    email = EmailField(
+        "Email Address", validators=[DataRequired(), Email(), Length(max=150)]
+    )
+
+    message = TextAreaField("Message", validators=[DataRequired(), Length(min=10)])
+
+    submit = SubmitField("Send Message")
