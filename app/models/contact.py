4d0e447b84302d78988679bb7a673d8da773b00c
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-18 21:21:58 +0430

    New model has been added for quick contact.

--- app/models/contact.py
+++ app/models/contact.py
@@ -4,11 +4,11 @@ from app.extensions import db
 class ContactMessage(db.Model):
     __tablename__ = "contact_messages"
 
-    first_name = db.Column(db.String(100), nullable=False)
-    last_name = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
     email = db.Column(db.String(150), nullable=False)
 
-    message = db.Column(db.Text, nullable=False)
+    message = db.Column(db.Text)
 
     def __repr__(self):
         return f"<ContactMessage {self.email}>"
