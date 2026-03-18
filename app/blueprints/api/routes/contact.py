ca413113e0e0c7e851d9db3a9e08a5d10abc03a4
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-18 21:22:09 +0430

    New endpoint has been added.

--- app/blueprints/api/routes/contact.py
+++ app/blueprints/api/routes/contact.py
@@ -4,7 +4,7 @@ from flask import jsonify
 
 from app.blueprints.api import bp
 from app.extensions import db
-from app.forms.contact import ContactForm
+from app.forms.contact import ContactForm, QuickContactForm
 from app.models.contact import ContactMessage
 
 
@@ -21,6 +21,33 @@ def contact():
         m.last_name = form.last_name.data
         m.email = form.email.data
         m.message = form.message.data
+
+        db.session.add(m)
+        db.session.commit()
+
+        response.update(
+            **{
+                "category": "success",
+                "message": "Message sent successfully",
+            }
+        )
+
+    else:
+        response["errors"] = form.errors
+
+    return jsonify(response)
+
+
+@bp.post("/quick-contact")
+def quick_contact():
+    form = QuickContactForm()
+
+    response: Dict = {}
+
+    if form.validate_on_submit():
+        m = ContactMessage()
+
+        m.email = form.email.data
 
         db.session.add(m)
         db.session.commit()
