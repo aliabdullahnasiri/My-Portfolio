6bd353dde26a355e673c0faf518ccabd04965ff1
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-19 12:59:15 +0430

    The nav_links list has been completed.

--- app/blueprints/main/routes/home.py
+++ app/blueprints/main/routes/home.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 from flask import render_template
 
 from app.blueprints.main import bp
@@ -8,13 +10,34 @@ from app.models.profile import Profile
 @bp.get("/")
 @bp.get("/home")
 def home():
+    profile = (
+        Profile.query.filter_by(is_active=True)
+        .order_by(getattr(Profile, "updated_at").desc())
+        .first()
+    )
+
+    nav_links: List[Tuple] = [("Home", "#home"), ("About", "#about")]
+
+    if hasattr(profile, "certificates") and getattr(profile, "certificates").count():
+        nav_links.append(("Certifications", "#certifications"))
+
+    if hasattr(profile, "projects") and getattr(profile, "projects").count():
+        nav_links.append(("Projects", "#projects"))
+
+    if hasattr(profile, "skills") and getattr(profile, "skills").count():
+        nav_links.append(("Skills", "#skills"))
+
+    if hasattr(profile, "education") and getattr(profile, "education").count():
+        nav_links.append(("Education", "#education"))
+
+    nav_links.append(("Contact", "#contact"))
+
     return render_template(
         "main/pages/home.html",
         title="Home",
         enumerate=enumerate,
-        profile=Profile.query.filter_by(is_active=True)
-        .order_by(getattr(Profile, "updated_at").desc())
-        .first(),
+        profile=profile,
         contact_form=ContactForm(),
         quick_contact_form=QuickContactForm(),
+        nav_links=nav_links,
     )
