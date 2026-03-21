bb3eeb815d47b22b9530a492b487d2c267fa79d2
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-21 13:24:10 +0430

    The notification service has been added.

---
+++ app/services/notification.py
@@ -0,0 +1,20 @@
+from app.extensions import db
+from app.models.notification import Notification, NotificationType
+from app.sockets.notification import notify_new_message
+
+
+def create_notification(
+    message: str, notif_type: NotificationType = NotificationType.INFO
+) -> Notification:
+    notification = Notification()
+
+    notification.message = message
+    notification.type = notif_type
+    notification.is_read = False
+
+    db.session.add(notification)
+    db.session.commit()
+
+    notify_new_message(notification)
+
+    return notification
