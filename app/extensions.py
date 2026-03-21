70be2f440db93e7475a1a0c10fee0823b897ea79
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-21 13:03:03 +0430

    Modified.

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,7 @@
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from rich.console import Console
 
@@ -10,6 +11,7 @@ migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 console = Console()
+socketio = SocketIO(cors_allowed_origins="*")
 
 login_manager.login_view = "admin.sign_in"
 login_manager.login_message_category = "info"
