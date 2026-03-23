1bb97db7073f6bd08ac25d06b7394a233dbb4f09
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-23 21:35:27 +0430

    Deleted.

--- app/sockets/notification.py
+++
@@ -1,6 +0,0 @@
-from app.extensions import socketio
-from app.models.notification import Notification
-
-
-def notify_new_message(notification: Notification):
-    socketio.emit("new_notification", notification.to_dict())
