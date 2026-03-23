8303cf4ee4058a5604d61bed216772aa59721d7a
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-03-23 21:22:37 +0430

    Modified.

--- app/sockets/notification.py
+++ app/sockets/notification.py
@@ -2,9 +2,5 @@ from app.extensions import socketio
 from app.models.notification import Notification
 
 
-@socketio.on("connect")
-def handle_connect(): ...
-
-
 def notify_new_message(notification: Notification):
     socketio.emit("new_notification", notification.to_dict())
